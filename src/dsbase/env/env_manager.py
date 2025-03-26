ebeada7f6b7213e4ed7a58f9200f9991854ecdbd
Danny Stewart <danny@stewart.cc>
2025-03-26 15:49:31 -0400

    EnvManager (refactor): Move EnvVar out to its own file

--- src/dsbase/env/env_manager.py
+++ src/dsbase/env/env_manager.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING, Any, ClassVar, TypeVar
 
 from dotenv import load_dotenv
 
+from dsbase.env.env_var import EnvVar
 from dsbase.log import LocalLogger
 from dsbase.util import Singleton
 
@@ -16,35 +17,6 @@ if TYPE_CHECKING:
     from logging import Logger
 
 T = TypeVar("T")
-
-
-@dataclass
-class EnvVar:
-    """Represents an environment variable with validation and type conversion.
-
-    Args:
-        name: Environment variable name.
-        required: Whether this variable is required.
-        default: Default value if not required.
-        var_type: Type to convert value to (e.g., int, float, str, bool).
-        description: Human-readable description of the variable.
-        secret: Whether to mask the value in logs.
-
-    NOTE: var_type is used as a converter function to wrap the provided data. This means it can also
-          use custom conversion functions to get other types of data with convert(value) -> Any.
-    """
-
-    name: str
-    required: bool = False
-    default: Any = None
-    var_type: Callable[[str], Any] = str
-    description: str = ""
-    secret: bool = False
-
-    def __post_init__(self) -> None:
-        if not self.required and self.default is None:
-            msg = f"Non-required variable {self.name} must have a default value"
-            raise ValueError(msg)
 
 
 @dataclass
